e74f2363276268720f008d1b561040f7ea9a8423
Nobuhide Tsuda <ntsuda@master.email.ne.jp>
2025-01-21 14:14:04 +0900

    0006 ◎ C 01/21 01/21 0.001│ToDo│Board.put_black, white(x, y) 0007 ◎ C 01/21 01/21 0.001│ToDo│Board.print()

--- hexgame/scripts/Board.gd
+++ hexgame/scripts/Board.gd
@@ -10,7 +10,17 @@ var m_cells = []
 
 func _init():
 	m_cells.resize(BD_SIZE)
-
+	for i in range(BD_SIZE):
+		m_cells[i] = []
+		m_cells[i].resize(BD_SIZE)
+		m_cells[i].fill(EMPTY)
+func print():
+	for i in range(BD_SIZE):
+		print(m_cells[i])
+func put_black(x, y):
+	m_cells[y][x] = BLACK
+func put_white(x, y):
+	m_cells[y][x] = WHITE
 func _ready():
 	pass # Replace with function body.
 func _process(delta):
--- hexgame/scripts/main.gd
+++ hexgame/scripts/main.gd
@@ -4,6 +4,9 @@ var bd
 
 func _ready():
 	bd = Board.new()
+	bd.put_black(0, 1)
+	bd.put_white(1, 0)
+	bd.print()
 	pass # Replace with function body.
 	
 func _process(delta):
