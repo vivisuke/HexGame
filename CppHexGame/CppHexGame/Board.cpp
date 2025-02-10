d90712c0635abaf0a6eafdda91d49a2c7772ff4c
Nobuhide Tsuda <ntsuda@master.email.ne.jp>
2025-02-11 07:43:22 +0900

    0089 ◎ C 02/11 02/11 0.001│ToDo│Board::print()：全角表示

--- CppHexGame/CppHexGame/Board.cpp
+++ CppHexGame/CppHexGame/Board.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 #include "Board.h"
 
 using namespace std;
@@ -12,21 +13,32 @@ Board::Board(int wd)
 	m_cells.resize(m_ary_size);
 	for(auto &v : m_cells) v = EMPTY;
 }
+const char *wstr[] = {"ÇÅ", "ÇÇ", "ÇÉ", "ÇÑ", "ÇÖ", "ÇÜ", "Çá", "Çà", "Çâ", "Çä", "Çã"};
 void Board::print() const {
-	cout << "   ";
-	for(int x = 0; x != m_width; ++x) cout << (char)('a'+x) << " ";
+	cout << "  ";
+	for(int x = 0; x != m_width; ++x) {
+		cout << wstr[x];
+		//wcout << wstring(1, ('ÇÅ' + x));
+		//cout << (char)('a'+x) << " ";
+	}
 	cout << endl;
 
 	for(int y = 0; y != m_width; ++y) {
 		//cout << y+1 << " ";
-		printf("%2d ", y+1);
+		printf("%2d", y+1);
 		for(int x = 0; x != m_width; ++x) {
 			auto ix = xyToIndex(x, y);
 			//cout << (int)m_cells[ix] << " ";
 			switch( m_cells[ix] ) {
+#if 1
+			case EMPTY: cout << "ÅE"; break;
+			case BLACK: cout << "Åú"; break;
+			case WHITE: cout << "Å¸"; break;
+#else
 			case EMPTY: cout << ". "; break;
 			case BLACK: cout << "X "; break;
 			case WHITE: cout << "O "; break;
+#endif
 			}
 		}
 		cout << endl;
